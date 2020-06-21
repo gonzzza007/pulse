cffffe38d247c2f15ed0d86e15cc285d88048523
gonzzza <gonzzza@gmail.com>
2020-06-21 16:40:57 +0300

    use training file locally 3

--- PULSE.py
+++ PULSE.py
@@ -20,8 +20,10 @@ class PULSE(torch.nn.Module):
         cache_dir = Path(cache_dir)
         cache_dir.mkdir(parents=True, exist_ok = True)
         if self.verbose: print("Loading Synthesis Network")
-        with open_url("https://drive.google.com/uc?id=1TCViX1YpQyRsklTVYEJwdbmK91vklCo8", cache_dir=cache_dir, verbose=verbose) as f:
-            self.synthesis.load_state_dict(torch.load(f))
+        # synthesis.pt
+        f = open('synthesis.pt')
+        # with open_url("https://drive.google.com/uc?id=1TCViX1YpQyRsklTVYEJwdbmK91vklCo8", cache_dir=cache_dir, verbose=verbose) as f:
+        self.synthesis.load_state_dict(torch.load(f))
 
         for param in self.synthesis.parameters():
             param.requires_grad = False
@@ -34,8 +36,10 @@ class PULSE(torch.nn.Module):
             if self.verbose: print("\tLoading Mapping Network")
             mapping = G_mapping().cuda()
 
-            with open_url("https://drive.google.com/uc?id=14R6iHGf5iuVx3DMNsACAl7eBr7Vdpd0k", cache_dir=cache_dir, verbose=verbose) as f:
-                    mapping.load_state_dict(torch.load(f))
+            # mapping.pt
+            f = open('mapping.pt')
+            # with open_url("https://drive.google.com/uc?id=14R6iHGf5iuVx3DMNsACAl7eBr7Vdpd0k", cache_dir=cache_dir, verbose=verbose) as f:
+            mapping.load_state_dict(torch.load(f))
 
             if self.verbose: print("\tRunning Mapping Network")
             with torch.no_grad():
